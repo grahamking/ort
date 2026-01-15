f99493d3adc4978ab4e1ddc113c1bb2b8fe024e9
Graham King <graham@gkgk.org>
2026-01-14 20:56:38 -0500

    Final tweaks before I try publishing it

--- build_release.sh
+++ build_release.sh
@@ -6,19 +6,11 @@
 # Hence we split the project into two crates, one that has all the weird stuff (cli/) and one that has all the code (core/) and tests.
 #
 
-# Set RUSTFLAGS here because:
-# 1. .cargo/config.toml [build.rustflags] is only used if
-# RUSTFLAGS is not set, and presumably most devs have it set.
-# https://doc.rust-lang.org/cargo/reference/config.html#buildrustflags
-# 2. .cargo/config.toml is used for both debug and release builds, but we need
-# it only for release builds.
-#
-# TODO: What about `cargo install` when we publish to crates.io?
-#
-RUSTFLAGS="-Ctarget-cpu=native -Crelocation-model=static -Clink-args=-Wl,--build-id=none,--no-eh-frame-hdr -Cforce-frame-pointers=yes"
-
 # Build std from source to avoid unwinding infrastructure. Save over 60 KiB and a dynamic link to libgcc_s.
-cargo build --release -Zbuild-std="core,alloc"
+cargo +nightly-2026-01-07 build --release -Zbuild-std="core,alloc"
+
+# After we publish
+# cargo +nightly-2026-01-07 install --locked -Zbuild-std="core,alloc" ort-openrouter-cli
 
 # Saves about 3 KiB
 if command -v llvm-strip >/dev/null 2>&1; then
